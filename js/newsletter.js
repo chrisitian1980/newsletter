14cbde9c891722204387c3f61fc6ed5e3e36f5ab
Christian <jchristianhaus@hotmail.com>
2018-02-12 22:12:43 -0600

    comenzamos con inputs

--- js/newsletter.js
+++ js/newsletter.js
@@ -5,6 +5,10 @@ var datos_contacto = {
 };
 $(document).ready(function(){
   $("#fila-2,#fila-3,#fila-4,#fila-5").hide();
+  $('#formulario').on('submit',function(event ){
+    event.preventDefault();
+    })
+    $('#fila-4').delegate('input','keyup',function(){});
   // Input de correo
 $("input#correo").on("keyup", function(){
   datos_contacto.correo = $("input#correo").val().trim();
@@ -29,6 +33,20 @@ if( datos_contacto.nombre ){
 $("input#numero").on("keyup", function(){
 var numero = parseInt($("input#numero").val().trim() );
 if( numero > 0 ){
+
+  $("#fila-4").html('');
+  var indice = 0;
+  while( indice < numero ){
+    $("#fila-4").append(
+      '<div class="col">'+
+      '<p>Un@ se llama</p>'+
+      '<input type="text" class="form-control">'+
+      '</div>'
+    );
+    indice++
+
+  }
+
   $("#fila-4").show();
 }else{
     $("#fila-4").hide();
